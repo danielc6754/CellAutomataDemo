bb7daed7e05862e0f1074434b51700c0b903de60
danielc6754 <daniel.c6754@gmail.com>
2019-05-09 07:05:44 -0700

    Completed Program

--- CellAuto.cpp
+++ CellAuto.cpp
@@ -21,27 +21,64 @@ public:
 		m_sAppName = L"Cellular Automata Demo";
 	}
 private:
+	// Two 2d arrays
+	// What is seen on screen
+	int* m_output;
+	// current state of cell
+	int* m_state;
 protected:
 	virtual bool OnUserCreate() {
+		m_output = new int[ScreenWidth() * ScreenHeight()];
+		m_state = new int[ScreenWidth() * ScreenHeight()];
+		memset(m_output, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
+		memset(m_state, 0, ScreenWidth() * ScreenHeight() * sizeof(int));
+
+		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+			m_state[i] = rand() % 2;
+
 		return true;
 	}
 
 	virtual bool OnUserUpdate(float fElapsedTime) {
-		// Count Neighbours
+		auto cell = [&](int x, int y) {
+			return m_output[y * ScreenWidth() + x];
+		};
 
-		// Rules if cell alive
-		// < 2 neighbours: dies from lonlies
-		//  > 3 neighbours: dies from overcrowding
-		// else stays alive
+		// Store output state
+		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+			m_output[i] = m_state[i];
 
-		// Rules for cell dead
-		// if dead cell has 3 neighbours, produce new cell 
+		// Iterate through cells; no boards to avoid reading mem that is not there
+		for (int x = 1; x < ScreenWidth() - 1; x++)
+			for (int y = 1; y < ScreenHeight() - 1; y++) {
+				// Count Neighbours
+				int nNeighbours = cell(x - 1, y - 1) + cell(x + 0, y - 1) + cell(x + 1, y - 1) +
+								  cell(x - 1, y + 0) +			0		  + cell(x + 1, y - 0) +
+								  cell(x - 1, y + 1) + cell(x + 0, y + 1) + cell(x + 1, y + 1);
+
+				// Rules if cell alive
+				// < 2 neighbours: dies from lonliness OR > 3 neighbours: dies from overcrowding
+				if (cell(x, y) == 1)
+					m_state[y * ScreenWidth() + x] = nNeighbours == 2 || nNeighbours == 3;
+				// Rules for cell dead
+				// if dead cell has 3 neighbours, produce new cell 
+				else
+					m_state[y * ScreenWidth() + x] = nNeighbours == 3;
+
+				// Draw
+				if (cell(x, y) == 1)
+					Draw(x, y, PIXEL_SOLID, FG_WHITE);
+				else
+					Draw(x, y, PIXEL_SOLID, FG_BLACK);
+			}
 		
 		return true;
 	}
 };
 
 int main() {
+	srand(clock());
+
 	GameofLife game;
 	game.ConstructConsole(80, 50, 8, 8);
 	game.Start();
